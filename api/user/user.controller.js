5e2d206953fc4cc77518841591955fb2491d0381
hyeon <kohy0329@blueprint-lab.com>
2018-05-03 04:17:49 +0900

    회원 생성 - 서버에 정보가 존재하면 생성과정 패스, 그렇지 않으면 저장

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -6,17 +6,24 @@ const { User } = require('../../models/user');
 
 // 회원 생성
 exports.create = (req, res) => {
-  User.create(req.body, (err, result) => {
-    if(!err) {
-      return res.json(result);
+// user code가 이미 서버 디비에 존재하는지 확인 : 없다면 생성, 있다면 패스
+  User.find({user_code :req.body.user_code}, function (err, user) {
+    if(err) return res.status(500).send('User 조회 failed');
+    console.log(user);
+    if(user=='') {
+      User.create(req.body, (err, result) => {
+        if(!err) {
+          return res.json(result);
+        }
+      }); // 존재하지 않는 회원 id는 새로 생성.
     }
-    return res.status(500).send(err);
+    else return res.status(500).json({ success : false , message : 'user already exist'});
   });
 };
 
 // 회원 프로필 관리
 exports.getProfile = (req, res) => {
-  User.findOne({user_code : req.params.id}, (err, result) => {
+  User.find({user_code : req.params.id}, (err, result) => {
     if (err) return res.status(500).send(err);
     return res.json(result);
   });
