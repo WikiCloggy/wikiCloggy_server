d937e797228496182a48c50f559473e3828ef982
hyeon <kohy0329@naver.com>
2018-06-04 03:02:15 +0900

    오류 수정

--- api/log/log.controller.js
+++ api/log/log.controller.js
@@ -133,13 +133,15 @@ exports.getAll = (req, res) => {
   });
 };
 
-const showPage = function(page) {
-  return Log.find({_id: req.params.id}).sort({"_id: -1 "}).skip((page-1)*npage).limit(npage);
-}
-
 // 한 페이지당 5개의 log 정보를 불러와서 return. sort 는 id 순으로.
 exports.getMore = (req, res) => {
-  return res.json({showPage(req.params.page)});
+  page = req.params.page;
+  Log.find({user_code: req.params.user_code}, function (err, result) {
+    if(!err) {
+      return res.json(result);
+    }
+    return res.status(500).send(err);
+  }).sort({_id: -1 }).skip((page-1)*npage).limit(npage);
 };
 
 // 로그 자세히 보기
